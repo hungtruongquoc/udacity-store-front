f75231fe30c07b49388c25829137f5c5a9ced09f
Hung Truong <hungtruongquoc@gmail.com>
2021-05-14 20:23:17 -0500

    20210514: Working on the product page

---
+++ src/app/app-routing.module.ts
@@ -0,0 +1,16 @@
+import { NgModule } from '@angular/core';
+import { RouterModule, Routes } from '@angular/router';
+
+const routes: Routes = [
+  { path: '',   redirectTo: '/products', pathMatch: 'full' }, // redirect to `first-component`
+  {
+    path: 'products',
+    loadChildren: () => import('./product/product.module').then(m => m.ProductModule)
+  }
+];
+
+@NgModule({
+  imports: [RouterModule.forRoot(routes)],
+  exports: [RouterModule]
+})
+export class AppRoutingModule { }
